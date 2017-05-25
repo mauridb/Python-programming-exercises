009ed814b370989d56bfd61a13891090c085d240
ColmeSH <mauriziocontatto@gmail.com>
2017-05-25 15:28:57 +0200

    update

---
+++ level_2/ex_9.py
@@ -0,0 +1,14 @@
+def solution(txt):
+  return txt.upper()
+
+
+
+while (1):
+  txt = raw_input()
+  if txt == '0':
+    break
+  else:
+    print solution(txt)
+    print 'if you wanna turn off the programm type 0'
+  
+  
